a14be19a97b14281e70360e1287e3d94ef8f2101
Ismael Damasceno Tristão <ismaeldamascenotris@gmail.com>
2023-08-26 10:52:15 -0300

    feat: sistema de dano

--- Project/Shadow Blasters/Assets/Objects/Player/JumpMember.cs
+++ Project/Shadow Blasters/Assets/Objects/Player/JumpMember.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 namespace Player
@@ -15,8 +16,11 @@ namespace Player
 		private BoxCollider2D _feetCollider;
 		private InputMember _inputMember;
 		private Rigidbody2D _rb;
+		public bool JumpControl = true;
 
 		private float _initialY;
+
+		private bool _startedJump = false;
 
 		private void Awake()
 		{
@@ -30,25 +34,39 @@ namespace Player
 
 		void Update()
 		{
+			if (!JumpControl)
+			{
+				return;
+			}
+
 			if (_inputMember.JumpingInput)
 			{
 				if (OnFloor() && _rb.velocity.y <= .03f)
 				{
 					_rb.velocity = new Vector2(_rb.velocity.x, _jumpStrenght);
 					_initialY = transform.position.y;
+					_startedJump = true;
+
+					Debug.Log("Setting Y by direct jump");
 				}
 			}
 			else
 			{
+				bool grounded = OnFloor();
 				float yDiff = transform.position.y - _initialY;
-				if (!OnFloor() && yDiff >= 1.4f && _rb.velocity.y >= 0f)
+				if (!grounded && yDiff >= 1.4f && _rb.velocity.y >= 0f && _startedJump)
 				{
+					Debug.Log("Setting Y by variable jump");
 					_rb.velocity = new Vector2(_rb.velocity.x, Mathf.Abs(_rb.velocity.y * 0.4f));
+				}
+				else if (grounded)
+				{
+					_startedJump = false;
 				}
 			}
 		}
 
-		private bool OnFloor()
+		public bool OnFloor()
 		{
 			return Physics2D.BoxCast(_feetCollider.transform.position, _feetCollider.size, 0, Vector2.down, 0.05f, _groundMask);
 		}
--- Project/Shadow Blasters/Assets/Objects/Player/PropertiesCore.cs
+++ Project/Shadow Blasters/Assets/Objects/Player/PropertiesCore.cs
@@ -14,25 +14,75 @@ namespace Player
 		[HideInInspector] public BoxCollider2D Collider;
 		[HideInInspector] public BoxCollider2D FeetCollider;
 
+		private MoveMember _moveMember;
+		private JumpMember _jumpMember;
+
 		public static GameObject Player;
+		public static PropertiesCore s_Instance;
+
+		public static float s_IvulnerableTime = 1f;
+		public static bool s_Ivulnerable = false;
 
 		[HideInInspector] public bool Attacking = false;
+
 
 		void Awake()
 		{
 			if (Player == null)
 			{
 				Player = gameObject;
+				s_Instance = this;
 
 				Rigidbody = GetComponent<Rigidbody2D>();
 				Collider = GetComponent<BoxCollider2D>();
 				FeetCollider = transform.GetChild(0).GetComponent<BoxCollider2D>();
+
+				_moveMember = GetComponent<MoveMember>();
+				_jumpMember = GetComponent<JumpMember>();
+
+				Physics2D.IgnoreLayerCollision(LayerMask.NameToLayer("Player"), LayerMask.NameToLayer("EnemySolid"), true);
 			}
 			else
 			{
 				Destroy(gameObject);
 				throw new UnityException("Só é permitida uma instância do Player por cena");
 			}
+		}
+
+		private void Update()
+		{
+			if (!_moveMember.enabled)
+			{
+				if (_jumpMember.OnFloor())
+				{
+					_moveMember.enabled = true;
+					_jumpMember.JumpControl = true;
+				}
+			}
+		}
+
+		public static void ApplyForce(Vector2 forceToApply)
+		{
+			s_Instance._moveMember.enabled = false;
+			s_Instance._jumpMember.JumpControl = false;
+
+			s_Instance.transform.position += Vector3.up * 0.1f;
+			s_Instance.Rigidbody.velocity = Vector2.zero;
+			s_Instance.Rigidbody.AddForce(forceToApply, ForceMode2D.Impulse);
+		}
+
+		public static void SetIvulnerable()
+		{
+			Physics2D.IgnoreLayerCollision(LayerMask.NameToLayer("Player"), LayerMask.NameToLayer("Enemy"), true);
+			s_Ivulnerable = true;
+			s_Instance.StartCoroutine(s_Instance.VulnerabilityCoroutine());
+		}
+
+		private IEnumerator VulnerabilityCoroutine()
+		{
+			yield return new WaitForSeconds(s_IvulnerableTime);
+			Physics2D.IgnoreLayerCollision(LayerMask.NameToLayer("Player"), LayerMask.NameToLayer("Enemy"), false);
+			s_Ivulnerable = false;
 		}
 	}
 }
