4b8ee1e0ab6f5b2860b7acfe1d82404172330948
Ismael Damasceno Tristão <ismaeldamascenotris@gmail.com>
2023-08-27 12:34:15 -0300

    fix: removendo sistema de espelhamento antigo (espelhamento por escala)

--- Project/Shadow Blasters/Assets/Objects/Player/MoveMember.cs
+++ Project/Shadow Blasters/Assets/Objects/Player/MoveMember.cs
@@ -12,33 +12,15 @@ namespace Player
 
 		[SerializeField] public float MoveSpeed;
 
-		private Vector2 _originalPlayerScale;
 		private InputMember _inputMember;
 		private Rigidbody2D _rigidbody;
-
-		private PropertiesCore _propsCore;
-		private DashMember _dashMember;
 
 		private void Awake()
 		{
             _inputMember = GetComponent<InputMember>();
-		}
-		void Start()
-		{
-			_propsCore = GetComponent<PropertiesCore>();
-			_dashMember = GetComponent<DashMember>();
-
 			_rigidbody = GetComponent<Rigidbody2D>();
-			_originalPlayerScale = transform.localScale;
 		}
 
-		private void Update()
-		{
-			if (_inputMember.MoveInput != 0f && !_propsCore.Attacking && !_dashMember.Dashing)
-			{
-				transform.localScale = new Vector2(_inputMember.MoveInput * _originalPlayerScale.x, _originalPlayerScale.y);
-			}
-		}
 		void FixedUpdate()
 		{
 			_rigidbody.velocity = new Vector2(MoveSpeed * _inputMember.MoveInput, _rigidbody.velocity.y);
